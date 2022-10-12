9fbde8faabb26e402069d8fc42e6070d57221f86
Himangshu Mahato <you@example.com>
2022-10-12 22:11:05 +0530

    signout&signup

---
+++ Signup/signup.js
@@ -0,0 +1,23 @@
+var login = JSON.parse(localStorage.getItem("........")) || [];
+document.querySelector("form").addEventListener("submit", myFun);
+function myFun() {
+  // document.getElementById("email").value = "";
+  // document.getElementById("password").value = "";
+  event.preventDefault();
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var flag = false;
+
+  for (var i = 0; i < login.length; i++) {
+    if (login[i].email === email && login[i].password === password) {
+      flag = true;
+    }
+  }
+  if (flag) {
+    alert("login Sucessfull ✔️");
+    window.location.href = "";
+  } else {
+    alert("Wrong Credential   ❌");
+  }
+}
+
