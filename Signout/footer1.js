c9fee984fd1685eb4368cda681a339d3b4a9de7b
Himangshu Mahato <you@example.com>
2022-10-13 22:15:30 +0530

    signup&signout

---
+++ Signout/footer1.js
@@ -0,0 +1,3 @@
+import footer from "../component/footer.js";
+console.log(footer)
+document.getElementById("footer").innerHTML=footer();
---
+++ Signup/footer2.js
@@ -0,0 +1,4 @@
+console.log("gfdgfg")
+import footer from "../component/footer.js";
+console.log(footer);
+document.getElementById("footer").innerHTML=footer();
