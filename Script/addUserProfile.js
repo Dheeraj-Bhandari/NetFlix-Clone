b31f9c418e96c78e6332d5dfef3259f04d0cb2ba
Shruti Joshi <shrutijoshi@Shrutis-MacBook-Pro.local>
2022-10-15 10:05:47 +0530

    done

---
+++ Script/addUserProfile.js
@@ -0,0 +1,10 @@
+const user = JSON.parse(localStorage.getItem("users")) || [];
+
+function displayProfile() {
+    event.preventDefault();
+    const name = {name: document.getElementById("addProfile-user-inputBox").value};
+    user.push(name);
+    localStorage.setItem("users", JSON.stringify(user));
+    location.href = "../Pages/Manage_profilesection.html"
+   
+}
