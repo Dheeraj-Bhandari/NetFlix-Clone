4d1881ecb8add4167f3f5192b83adf2801b52a42
Ashish Saini <aashucsengi@gmail.com>
2022-10-16 09:11:54 +0530

    done with the input correctness part

--- aashu_scripts/credit.js
+++ aashu_scripts/credit.js
@@ -25,3 +25,54 @@ document.querySelector("#right_menu").addEventListener("click",()=>{
     event.preventDefault();
     window.location.href="./Signout/signout.html";
 })
+
+let fname=document.querySelector("#fname").value;
+if(fname!=null){
+   let ema= document.querySelector("#ema");
+   ema.style.transform="translateY(-0.8em)";
+}
+else{
+    let ema= document.querySelector("#ema");
+   
+   ema.style.transform="none";
+}
+let lname=document.querySelector("#lname").value;
+if(lname!=null){
+    let ema= document.querySelector("#lna");
+    ema.style.transform="translateY(-0.8em)";
+ }
+ else{
+     let ema= document.querySelector("#lna");
+    
+    ema.style.transform="none";
+ }
+let card_num=document.querySelector("#card_num").value;
+if(card_num!=null){
+    let ema= document.querySelector("#card");
+    ema.style.transform="translateY(-0.8em)";
+ }
+ else{
+     let ema= document.querySelector("#card");
+    
+    ema.style.transform="traslatY(0.8em)";
+ }
+let expiration=document.querySelector("#expiration").value;
+if(expiration!=null){
+    let ema= document.querySelector("#expir");
+    ema.style.transform="translateY(-0.8em)";
+ }
+ else{
+     let ema= document.querySelector("#expir");
+    
+    ema.style.transform="none";
+ }
+let cvv=document.querySelector("#cvv").value;
+if(cvv!=null){
+    let ema= document.querySelector("#cv");
+    ema.style.transform="translateY(-0.8em)";
+ }
+ else{
+     let ema= document.querySelector("#cv");
+    
+    ema.style.transform="none";
+ }
